c2dd3b0316453e60de494b0cd3744c9f7a89ae25
Carolimaa <carolniver12@gmail.com>
2024-01-17 16:55:01 -0300

    Criando as Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import Grid from './components/Grid/Grid';
+import Home from './pages/home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-   <Grid/>
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
-
-
-
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,31 @@
+import homeLogo from '../../assets/blogreact.png'
+import './Home.css';
+
+
 function Home() {
     return (
-      <div className="
-       border-4 border-[#dc143c]
-       m-8 p-8
-       flex items-center justify-center">
+        <>
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
-          <div className="w-[250px]">
-              <h1>Teresa Mendonça</h1>
-              <p>Narco mexicana más poderosa de España</p>
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
+            </div>
           </div>
-  
-          <div className="w-[250px]">
-              <img src="https://www.diariodepernambuco.com.br/static/app/noticia_127983242361/2015/08/18/592949/20150818090107838935i.jpg"
-              alt="Tereza Mendonça"/>
-          </div>
-      </div>
-    )
-  }
-  
-  export default Home
+        </div>
+      
+      </>
+    );
+}
+
+export default Home;
