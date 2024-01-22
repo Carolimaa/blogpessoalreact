2cf6084f86d302440fde71e9e3bf096b30ec1d39
Carolimaa <carolniver12@gmail.com>
2024-01-22 16:08:29 -0300

    Implementando os CRUDS

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Cadastro from './pages/Cadastro/Cadastro';
-import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 
+import Login from './pages/Login/Login'
+import Cadastro from './pages/Cadastro/Cadastro'
+import Home from './pages/Home/Home'
+import ListaTemas from './components/Temas/ListaTemas/ListaTemas'
+import FormularioTema from './components/Temas/FormularioTema/FormularioTema'
+import DeletarTema from './components/Temas/DeletarTema/DeletarTema'
 
 function App() {
-  return (
-    <>
-    <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
-    </>
-  );
+    return (
+        <AuthProvider>  {}
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>  {}
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastroTema" element={<FormularioTema />} />
+                        <Route path="/editarTema/:id" element={<FormularioTema />} />   {}
+                        <Route path="/deletarTema/:id" element={<DeletarTema />} />     {}
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </ AuthProvider>
+    )
 }
-export default App;
+
+export default App
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,36 @@
-import { Link, useNavigate } from 'react-router-dom'
-import React from 'react'
-
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar() {
- 
   
+    const navigate = useNavigate()
 
-  return (
-    <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+    const { handleLogout } = useContext(AuthContext)
 
-            <div className='flex gap-4'>
-              <Link to='/login' className='hover:underline'>Login</Link>
-              <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+    function logout() {
+        handleLogout()
+        alert('Usuário deslogado com sucesso')
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        <div className='hover:underline'>Postagens</div>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+                        <div className='hover:underline'>Perfil</div>
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
